269abf9cc58dfaf4db8df737f31c1b26fb239038
Brock Frazier <15bfrazier2@gmail.com>
2020-02-27 18:47:47 -0600

    Practice with let/where

--- learn.hs
+++ learn.hs
@@ -7,3 +7,10 @@ module Learn where
     myResult = x * 5
 
     y = 10
+
+    printInc n = print plusTwo
+        where plusTwo = n + 2
+
+    printInc2 n = let plusTwo = n + 2
+                  in print plusTwo
+    
